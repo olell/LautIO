d93060d5b12f720188d551a73b19490e1b8a0ebc
olel <olel@olel.de>
2021-04-02 00:01:28 +0200

    Updated pin definition names

--- Software/LautIO/src/hardware/hw_proto1.h
+++ Software/LautIO/src/hardware/hw_proto1.h
@@ -11,19 +11,19 @@
 #pragma once
 
 // Amp AB Controls
-#define AMP_AB_MUTE    17 // IO17
-#define AMP_AB_STANDBY 16 // IO16
+#define AMP_AB_MUTE_PIN    17 // IO17
+#define AMP_AB_STANDBY_PIN 16 // IO16
 
 // Amp CD Controls
-#define AMP_CD_MUTE    33 // IO33
-#define AMP_CD_STANDBY 32 // IO32
+#define AMP_CD_MUTE_PIN    33 // IO33
+#define AMP_CD_STANDBY_PIN 32 // IO32
 
 // DSP Controls
-#define DSP_RESET      14 // IO14
+#define DSP_RESET_PIN      14 // IO14
 
 // I2S Output
-#define DSP_IN_DATA    26 // IO26
-#define DSP_IN_BCLK    27 // IO27
-#define DSP_IN_LRCLK   13 // IO13
+#define DSP_IN_DATA_PIN    26 // IO26
+#define DSP_IN_BCLK_PIN    27 // IO27
+#define DSP_IN_LRCLK_PIN   13 // IO13
 
 //#define HARDWARE_HAS_SDCARD // Uncomment on hardware versions which have sd card slots
