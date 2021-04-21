0044848e452d1b9277d496366fd942058a69ac53
olel <olel@olel.de>
2021-04-21 17:42:14 +0200

    Added license header to main file

--- Software/LautIO/src/main.cpp
+++ Software/LautIO/src/main.cpp
@@ -1,3 +1,13 @@
+/*
+ *
+ * This file is part of LautIO
+ * 
+ * Copyright 2021 - olel
+ * LautIO is licensed under MIT License
+ * See LICENSE file for more information
+ * 
+ */
+
 #include <Arduino.h>
 
 void setup() {
