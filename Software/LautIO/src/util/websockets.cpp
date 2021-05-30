17eb707e196d0cde7841fc9de815256d23b2d742
olel <olel@olel.de>
2021-05-30 19:09:09 +0200

    Fixed this sh*tty memory error and some code improvement

--- Software/LautIO/src/util/websockets.cpp
+++ Software/LautIO/src/util/websockets.cpp
@@ -21,17 +21,6 @@ unsigned long ws_last_cleanup_millis = 0;
 
 void handle_websocket_data(const char* message, AsyncWebSocketClient* client) {
     /* This method handles all incoming websocket data (on websocket 0.0.0.0/ws) */
-    log_debug("Handling websocket data");
-    DynamicJsonDocument json_document(1024);
-    log_debug("Created json doc");
-    DeserializationError e = deserializeJson(json_document, message);
-    if (e) {
-        log_debug("Websocket json parsing error %s!", e.c_str());
-        client->text("JSON Error!");
-        return;
-    }
-    log_debug("parsed json");
-    client->text(handle_interfaces(json_document));
 }
 
 void on_websocket_event(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len) {
@@ -40,15 +29,15 @@ void on_websocket_event(AsyncWebSocket* server, AsyncWebSocketClient* client, Aw
         client->text("{\"status\": \"ok\", \"message\": \"connection successfully established!\"}");
     }
     else if (type == WS_EVT_DATA) {
-        char* message;
-        message = (char*) malloc(len + 1);
-        memcpy(message, data, len);
-        message[len] = 0; // String terminator
-
-        log_debug("Received data: %s", message);
-        handle_websocket_data(message, client);
-
-        free(message);
+        DynamicJsonDocument json_document(1024);
+        DeserializationError e = deserializeJson(json_document, data);
+        if (e) {
+            log_debug("Websocket json parsing error %s!", e.c_str());
+            client->text("JSON Error!");
+            return;
+        }
+        log_debug("parsed json");
+        client->text(handle_interfaces(json_document));
     }
     else if (type == WS_EVT_DISCONNECT) {
         log_debug("Websocket client disconnected");
