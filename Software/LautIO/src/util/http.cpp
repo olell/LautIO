6e8d57aa1d2cbab4d7d3247bdcf6580b6a862315
olel <olel@olel.de>
2021-07-17 14:22:27 +0200

    Implemented custom webui serving

--- Software/LautIO/src/util/http.cpp
+++ Software/LautIO/src/util/http.cpp
@@ -21,14 +21,28 @@
 
 AsyncWebServer webserver(HTTP_PORT);
 
+void serve_web_ui(AsyncWebServerRequest *request) {
+    const char* file_name = request->pathArg(0).c_str();
+    char* sd_path = (char*) malloc(5 + strlen(file_name)); // len("/web/") == 5
+    sprintf(sd_path, "/web/%s", file_name);
+
+    if(FSHANDLE.exists(sd_path)){
+        AsyncWebServerResponse *response = request->beginResponse(FSHANDLE, sd_path);
+        response->addHeader("Cache-Control", "max-age=31536000");
+        request->send(response);
+    } else request->send(404);
+}
+
+
 void init_http() {
     log_info("Initialising HTTP"); 
 
-    // Routes
-    webserver.serveStatic("/", FSHANDLE, "/web/").setDefaultFile("index.html").setCacheControl("max-age=600");;
-
     // Handler
     init_websocket(&webserver);
+
+    // Routes
+    webserver.on("^\\/(.*)$", HTTP_GET, serve_web_ui);
+    webserver.rewrite("/", "/index.html");
 
     // Start webserver
     webserver.begin();
