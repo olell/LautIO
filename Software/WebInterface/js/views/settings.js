2d31019855930129beb9fd981bbd7f359b95d841
olel <olel@olel.de>
2021-10-12 14:20:48 +0200

    Added settings page w/ debug button

--- Software/WebInterface/js/views/settings.js
+++ Software/WebInterface/js/views/settings.js
@@ -1,11 +1,18 @@
 class SettingsView {
-
     static render() {
-
+        render_template("templates/settings.html", {
+            "lautio": lautio,
+        },
+        function (html) {
+            $("#main_container").html(html);
+        });
     }
 
     static update_values() {
 
     }
+}
 
+function reboot() {
+    lautio.reboot();
 }
